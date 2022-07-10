adab8a0de2dad102dfec3c5f7000457bc99b64d6
Bill Schumacher <bill@servernet.co>
2022-07-10 18:57:27 -0500

    Add some comments around reasoning for the selected packages. Added base64, cgi, logging, and shelve. Avoided race condition in logging including file name in output and improved found import collection, for future use.

--- deepsource/tasks/scan_file.py
+++ deepsource/tasks/scan_file.py
@@ -1,4 +1,6 @@
 import itertools
+from collections import defaultdict
+
 from deepsource.celery_app import app
 from deepsource.util.file_operations import parse_code, get_file_content
 
@@ -14,20 +16,40 @@ def scan_file(file_path):
     )
     # print(imports)
     sus_imports = [
-        'pickle',
-        'ast',
-        'importlib',
-        'shlex',
-        'subprocess',
-        'sys',
+        # https://datatracker.ietf.org/doc/html/rfc4648.html#section-12
+        'base64',      # Hide's strings and more.
+        'cgi',         # Can be used to execute arbitrary commands.
+        'logging',     # Logging Configuration uses eval()
+        'pickle',      # Pickle can be used to execute arbitrary code.
+        'ast',         # AST can be used to execute arbitrary code.
+        'importlib',   # Importlib can be used to execute arbitrary code.
+        'shelve',      # Shelve can be used to execute arbitrary code, uses pickle.
+        'shlex',       # Shlex can be used to execute arbitrary code/shell commands.
+        'subprocess',  # Subprocess can be used to execute arbitrary code/shell commands.
+        'sys',         # sys.modules/meta_path/paths might have some risks.
+        # https://docs.python.org/3/library/tempfile.html#tempfile-mktemp-deprecated
         'tempfile',
-        'zipfile'
+        # The XML processing modules are not secure against maliciously constructed data.
+        # An attacker can abuse XML features to carry out denial of service attacks,
+        # access local files, generate network connections to other machines,
+        # or circumvent firewalls.
+        # https://docs.python.org/3/library/xml.html#xml-vulnerabilities
+        'xml',
+        # https://docs.python.org/3/library/zipfile.html#zipfile-resources-limitations
+        'zipfile'       # zipfile can be exploited to consume disk, memory, and CPU.
     ]
+    found_imports = defaultdict(list)
     for (key, value), _import in itertools.product(imports.items(), sus_imports):
         if not _import:
             continue
         if (key and _import in key) or (value and _import in value):
-            print(f"Suspicious import: [{_import}] @ line(s) {[_imports[0] for _imports in value]}.")
+            found_imports[_import].extend(tuple(_imports[0] for _imports in value))
+
+    for key in found_imports:
+        found_imports[key] = list(set(found_imports[key]))
+
+    for _import, lines in found_imports.items():
+        print(f"Suspicious import: [{_import}] @ {file_path} line(s) {lines}.")
     # print(calls)
     # print(assignments)
     if syntax_error:
--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
         "Topic :: Security",
     ],
     install_requires=["celery", "redis"],
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
